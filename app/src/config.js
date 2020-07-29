8959b689a60a84aa86fcd872446d2f3e3d9aa911
Delton <delton.watkins@selu.edu>
2020-07-28 23:48:57 -0500

    added config

---
+++ app/src/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "us-east-1_pE9GfNUHL",
+    userPoolClientId: "47h3fuhoi063nvfsb0oi00skhd",
+    region: "us-east-1",
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+  },
+};
